1c1c579b4386bfebbb1e033521567797d3f02913
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-20 23:55:17 +0300

    added reschedule page and venues

---
+++ client/src/pages/Reschedule/reschedule.jsx
@@ -0,0 +1,13 @@
+import Sidebar from '../../components/Sidebar'
+import Navbar from '../../components/Navbar'
+
+const Reschedule = () => {
+    return (
+        <div className='flex'>
+            <Sidebar />
+            <Navbar />
+        </div>
+    )
+}
+
+export default Reschedule
