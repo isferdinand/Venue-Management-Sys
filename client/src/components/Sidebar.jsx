e87c1ada5a3fb206ecf744d25e25a4d545a511f7
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-20 23:53:11 +0300

    added usestate to toggle farbars, with props

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,46 +1,49 @@
-// import React from 'react'
 import { FaHome, FaCog, FaPlus, FaEdit } from 'react-icons/fa'
 import { FaBuildingColumns } from 'react-icons/fa6'
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
 
 
-const Sidebar = () => {
-
+const Sidebar = ({ sidebarToggle }) => {
+    Sidebar.propTypes = {
+        sidebarToggle: PropTypes.bool.isRequired
+    }
     return (
-        <div className={`w-64 bg-gray-800 fixed h-full px-4 py-2`}>
+        <div className={`${sidebarToggle ? "hidden" : "block"} w-64 bg-gray-800 fixed h-full px-4 py-2`}>
             <div className='my-2 mb-3'>
                 <h1 className="text-xl text-white font-bold">Admin Dashboard</h1>
             </div>
             <hr />
             <ul className='mt-3 text-white font-bold'>
                 <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                    <Link to={'/'} className='px-3'>
                         <FaHome className='inline-block w-6 h-6 mr-2 -mt-2'></FaHome>
                         Home
-                    </a>
+                    </Link>
                 </li>
                 <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                    <Link to={'/addevent'} className='px-3'>
                         <FaPlus className='inline-block w-6 h-6 mr-2 -mt-2' />
                         New Event
-                    </a>
+                    </Link>
                 </li>
                 <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                    <Link to={'/reschedule'} className='px-3'>
                         <FaEdit className='inline-block w-6 h-6 mr-2 -mt-2' />
                         Reschedule
-                    </a>
+                    </Link>
                 </li>
                 <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                    <Link to={'/venues'} className='px-3'>
                         <FaBuildingColumns className='inline-block w-6 h-6 mr-2 -mt-2' />
                         Venues
-                    </a>
+                    </Link>
                 </li>
                 <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-                    <a href="" className='px-3'>
+                    <Link to={'/'} className='px-3'>
                         <FaCog className='inline-block w-6 h-6 mr-2 -mt-2' />
                         Profile & Setting
-                    </a>
+                    </Link>
                 </li>
             </ul>
         </div>
