d908aa7096110a7c729952c48122f3769ccc0592
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-21 19:03:04 +0300

    added button  and changed bg form color

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -2,6 +2,12 @@ import Sidebar from '../../components/Sidebar'
 import Navbar from '../../components/Navbar'
 import { useState } from 'react'
 import { FaSearch } from "react-icons/fa"
+import room1 from '../../assets/Room1.jpg'
+import room2 from '../../assets/Room2.jpg'
+import Hall1 from '../../assets/Hall1.jpg'
+import Hall2 from '../../assets/Hall2.jpg'
+
+
 
 const Home = () => {
     const [sidebarToggle, setSidebarToggle] = useState(false)
@@ -21,27 +27,57 @@ const Home = () => {
                     </span>
                     <input type="text" name="search" id="search" placeholder='Search event...' className="w-3/4 px-4 py-1 pl-12 border-b-2 border-b-gray-600 rounded shadow outline-none hidden md:block mt-2" />
                 </div>
-                <div className='flex flex-col gap-3'>
-                    <div className='flex gap-2 border-2 border-red-500'>
-                        <h3 className='text-lg font-bold'>Hall A</h3>
-                        <h3>Lecture</h3>
-                        <h3>12th Feb 2023, 10:00</h3>
+                <div className='flex flex-col items-center gap-3'>
+
+                    <div className='flex gap-4 items-center border-2 border-gray-400 p-2 w-3/4'>
+                        <div>
+                            <img src={room1} alt="event" className=' max-w-24' />
+                        </div>
+                        <div className="flex flex-col">
+                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h3>Lecture</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste veritatis officia a voluptatem odio corrupti!</p>
+                            <p>Estimated Attendees: 50</p>
+                            <h3>12th Feb 2023, 10:00</h3>
+                        </div>
                     </div>
-                    <div className='flex gap-2 border-2 border-red-500'>
-                        <h3 className='text-lg font-bold'>Hall A</h3>
-                        <h3>Lecture</h3>
-                        <h3>12th Feb 2023, 10:00</h3>
+                    <div className='flex gap-4 items-center border-2 border-gray-400  p-2 w-3/4'>
+                        <div>
+                            <img src={room2} alt="event" className=' max-w-24' />
+                        </div>
+                        <div className="flex flex-col">
+                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h3>Workshop</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatem voluptatum atque inventore? Delectus, corrupti?</p>
+                            <p>Estimated Attendees: 200</p>
+                            <h3>12th Feb 2023, 10:00</h3>
+                        </div>
                     </div>
-                    <div className='flex gap-2 border-2 border-red-500'>
-                        <h3 className='text-lg font-bold'>Hall A</h3>
-                        <h3>Lecture</h3>
-                        <h3>12th Feb 2023, 10:00</h3>
+                    <div className='flex gap-4 items-center border-2 border-gray-400  p-2 w-3/4'>
+                        <div>
+                            <img src={Hall2} alt="event" className=' max-w-24' />
+                        </div>
+                        <div className="flex flex-col">
+                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h3>Performance</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, possimus voluptates nostrum accusamus reprehenderit tempore?</p>
+                            <p>Estimated Attendees: 1000</p>
+                            <h3>12th Feb 2023, 10:00</h3>
+                        </div>
                     </div>
-                    <div className='flex gap-2 border-2 border-red-500'>
-                        <h3 className='text-lg font-bold'>Hall A</h3>
-                        <h3>Lecture</h3>
-                        <h3>12th Feb 2023, 10:00</h3>
+                    <div className='flex gap-4 items-center border-2 border-gray-400  p-2 w-3/4'>
+                        <div>
+                            <img src={Hall1} alt="event" className=' max-w-24' />
+                        </div>
+                        <div className="flex flex-col">
+                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h3>Conference</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolor!</p>
+                            <p>Estimated Attendees: 400</p>
+                            <p>12th Feb 2023, 10:00</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
