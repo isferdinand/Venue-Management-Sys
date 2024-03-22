2bbe361e09ba950115bfc189a3fe6d9a950f6b46
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-22 15:55:56 +0300

    updated home page

--- client/src/pages/Home/home.jsx
+++ client/src/pages/Home/home.jsx
@@ -29,43 +29,52 @@ const Home = () => {
                 </div>
                 <div className='flex flex-col items-center gap-3'>
 
-                    <div className='flex gap-4 items-center border-2 border-gray-400 p-2 w-3/4'>
+                    <div className='flex gap-4 items-center justify-evenly rounded-md border-2 border-gray-400 p-2 w-4/5'>
                         <div>
                             <img src={room1} alt="event" className=' max-w-24' />
                         </div>
                         <div className="flex flex-col">
-                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h2 className='text-lg font-bold'>Room 1</h2>
                             <h3>Lecture</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste veritatis officia a voluptatem odio corrupti!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste veritatis officia a </p>
                             <p>Estimated Attendees: 50</p>
                             <h3>12th Feb 2023, 10:00</h3>
                         </div>
-                    </div>
-                    <div className='flex gap-4 items-center border-2 border-gray-400  p-2 w-3/4'>
                         <div>
-                            <img src={room2} alt="event" className=' max-w-24' />
+                            <button className='outline:none p-1 bg-red-500 rounded-md text-white font-semibold'>Cancel Event</button>
+                        </div>
+                    </div>
+                    <div className='flex gap-4 items-center justify-evenly rounded-md border-2 border-gray-400  p-2 w-4/5'>
+                        <div className=''>
+                            <img src={room2} alt="event" className='max-w-24' />
                         </div>
                         <div className="flex flex-col">
-                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h2 className='text-lg font-bold'>Room 2</h2>
                             <h3>Workshop</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatem voluptatum atque inventore? Delectus, corrupti?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatem </p>
                             <p>Estimated Attendees: 200</p>
                             <h3>12th Feb 2023, 10:00</h3>
                         </div>
+                        <div>
+                            <button className='outline:none p-1 bg-red-500 rounded-md text-white font-semibold'>Cancel Event</button>
+                        </div>
                     </div>
-                    <div className='flex gap-4 items-center border-2 border-gray-400  p-2 w-3/4'>
+                    <div className='flex gap-4 items-center justify-evenly rounded-md border-2 border-gray-400  p-2 w-4/5'>
                         <div>
                             <img src={Hall2} alt="event" className=' max-w-24' />
                         </div>
                         <div className="flex flex-col">
-                            <h2 className='text-lg font-bold'>Hall A</h2>
+                            <h2 className='text-lg font-bold'>Hall B</h2>
                             <h3>Performance</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, possimus voluptates nostrum accusamus reprehenderit tempore?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, possimus ?</p>
                             <p>Estimated Attendees: 1000</p>
                             <h3>12th Feb 2023, 10:00</h3>
                         </div>
+                        <div>
+                            <button className='outline:none p-1 bg-red-500 rounded-md text-white font-semibold'>Cancel Event</button>
+                        </div>
                     </div>
-                    <div className='flex gap-4 items-center border-2 border-gray-400  p-2 w-3/4'>
+                    <div className='flex gap-4 items-center justify-evenly rounded-md border-2 border-gray-400  p-2 w-4/5'>
                         <div>
                             <img src={Hall1} alt="event" className=' max-w-24' />
                         </div>
@@ -75,6 +84,9 @@ const Home = () => {
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, dolor!</p>
                             <p>Estimated Attendees: 400</p>
                             <p>12th Feb 2023, 10:00</p>
+                        </div>
+                        <div>
+                            <button className='outline:none p-1 bg-red-500 rounded-md text-white font-semibold'>Cancel Event</button>
                         </div>
                     </div>
 
