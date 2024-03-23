ce6fd5ad225c92620c5400e6fd2532d839a93454
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-23 09:42:12 +0300

    post method on the loginuser page

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Starts the server and listens for incoming requests on port 3002.
+app.listen(3002, () => {
+  console.log('Server is running at port 3002');
+});
+
+// Creating a db connection
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: '',
+  database: 'venue_management_sys',
+});
+
+app.post('/login', (req, res) => {
+  const sentUserName = req.body.LoginUserName;
+  const sentPassword = req.body.LoginPassword;
+
+  const SQL = 'SELECT * FROM users WHERE userName = ? && password = ?';
+
+  const Values = [sentUserName, sentPassword];
+
+  db.query(SQL, Values, (err, result) => {
+    if (err) {
+      res.send({ err });
+    }
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      res.send({ message: 'Wrong email or password!' });
+    }
+  });
+});
