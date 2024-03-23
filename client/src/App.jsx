2d66d156e7d455c4b696acd1af900f6d896a8bf0
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-23 16:56:30 +0300

    changed the root page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Login />,
       errorElement: <NotFound />
     },
     {
