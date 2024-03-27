63d21a6f182b350b235f4dde6b3fa4101dae77d1
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-27 23:14:54 +0300

    added change password functionality

--- client/src/pages/ProfileSet/profile.jsx
+++ client/src/pages/ProfileSet/profile.jsx
@@ -2,12 +2,38 @@ import Sidebar from '../../components/Sidebar'
 import Navbar from '../../components/Navbar'
 import PropTypes from 'prop-types'
 import { useState } from 'react'
+import Axios from 'axios'
 
 const Profile = () => {
     Profile.propTypes = {
         sidebarToggle: PropTypes.bool.isRequired
     }
     const [sidebarToggle, setSidebarToggle] = useState(false)
+    const [currentPassword, setCurrentPassword] = useState('')
+    const [newPassword, setNewPassword] = useState('')
+    const [confirmNewPassword, setConfirmNewPassword] = useState('')
+
+    const changePassword = (e) => {
+        e.preventDefault()
+
+        Axios.post('http://localhost:3002/profile', {
+            currentPassword: currentPassword,
+            newPassword: newPassword,
+            confirmNewPassword: confirmNewPassword
+        }).then((response) => {
+            if (response.data.message) {
+                alert(response.data.message)
+            }
+            console.log(response);
+        }).catch((error) => {
+            console.error(error);
+        });
+
+        if (newPassword !== confirmNewPassword) {
+            alert("The new passwords don't match");
+            return;
+        }
+    }
 
     return (
         <div>
@@ -21,12 +47,24 @@ const Profile = () => {
                 <div className='ml-6'>
                     <form action="">
                         <label htmlFor="Current password" className='block w-full font-semibold '>Current password</label>
-                        <input type="text" className='border-2 border-gray-300 rounded-lg p-1 m-1 outline-none' />
+                        <input type="password" className='border-2 border-gray-300 rounded-lg p-1 m-1 outline-none'
+                            value={currentPassword}
+                            onChange={(e) => { setCurrentPassword(e.target.value) }}
+                            autoComplete='currentPassword'
+                        />
                         <label htmlFor="New password" className='block w-full font-semibold'>New password</label>
-                        <input type="text" className='border-2 border-gray-300 rounded-lg p-1  outline-none m-1' />
+                        <input type="password" className='border-2 border-gray-300 rounded-lg p-1  outline-none m-1'
+                            value={newPassword}
+                            onChange={(e) => { setNewPassword(e.target.value) }}
+                            autoComplete='newPassword'
+                        />
                         <label htmlFor="Confirm new password" className='block w-full font-semibold'>Confirm new password</label>
-                        <input type="text" className='border-2 border-gray-300 rounded-lg p-1 m-1 block  outline-none' />
-                        <button className='mt-2 bg-blue-500 text-white px-3 py-2 px m-1 rounded-lg hover:bg-blue-200'>Change Password </button>
+                        <input type="password" className='border-2 border-gray-300 rounded-lg p-1 m-1 block  outline-none'
+                            value={confirmNewPassword}
+                            onChange={(e) => { setConfirmNewPassword(e.target.value) }}
+                            autoComplete='confirmNewPassword'
+                        />
+                        <button className='mt-2 bg-blue-500 text-white px-3 py-2 px m-1 rounded-lg hover:bg-blue-200' onClick={changePassword}>Change Password </button>
                     </form>
                 </div>
             </div>
