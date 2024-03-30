37ea21d94bbf4199ff365bf9a32cb0fd6ca49603
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-30 15:08:16 +0300

    displayed venues dynamically from the db

--- client/src/pages/Venues/venues.jsx
+++ client/src/pages/Venues/venues.jsx
@@ -1,6 +1,7 @@
 import Sidebar from '../../components/Sidebar'
 import Navbar from '../../components/Navbar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import PropTypes from 'prop-types'
 
 import room1 from '../../assets/Room1.jpg'
@@ -12,10 +13,33 @@ import Hall3 from '../../assets/Hall3.jpg'
 
 const Venues = () => {
     const [sidebarToggle, setSidebarToggle] = useState(false)
+    const [venues, setVenues] = useState([]);
+
     Venues.propTypes = {
         setSidebarToggle: PropTypes.func.isRequired,
         sidebarToggle: PropTypes.bool.isRequired
     }
+
+    const venueImages = {
+        'Room 1': room1,
+        'Room 2': room2,
+        'Room 3': room3,
+        'Hall 1': Hall1,
+        'Hall 2': Hall2,
+        'Hall 3': Hall3
+    };
+
+    useEffect(() => {
+        axios.get('http://localhost:3002/venues')
+            .then(response => {
+                setVenues(response.data);
+                // console.log(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []);
+
     return (
         <div>
             <div className="flex">
@@ -26,54 +50,20 @@ const Venues = () => {
             <div className="ml-64 flex flex-col items-center mt-10">
                 <h1 className="text-3xl font-bold mb-2">Venues</h1>
                 <div className='flex flex-wrap gap-3 ml-6'>
-                    <div className='p-2 border-2 border-gray-600 rounded-sm'>
-                        <img src={room1} alt="room1" className=' w-96' />
-                        <div>
-                            <h2>Location: Building A</h2>
-                            <p>Capacity: 200</p>
-                            <p>Status: Booked</p>
+                    {venues.map(venue => (
+                        <div className='p-2 border-2 border-gray-600 rounded-sm' key={venue.venueID}>
+                            {/* <img src={room1} alt="room1" className=' w-96' /> */}
+                            <img src={venueImages[venue.venueName]} alt={venue.venueName} className='w-96' />
+                            <div>
+                                <div>
+                                    <h2>Venue Name: <span className='font-semibold'>{venue.venueName}</span></h2>
+                                    <h2>Location: <span className='font-semibold'>{venue.location}</span></h2>
+                                    <p>Capacity: <span className='font-semibold'>{venue.capacity} </span> </p>
+                                    <p>Status: <span className='font-semibold'>{venue.Status}</span> </p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className='border-2 border-gray-600 p-2 rounded-sm'>
-                        <img src={room3} alt="room3" className='w-96' />
-                        <div>
-                            <h2>Location: Building A</h2>
-                            <p>Capacity: 400</p>
-                            <p>Status: Vacant</p>
-                        </div>
-                    </div>
-                    <div className='border-2 border-gray-600 p-2 rounded-sm'>
-                        <img src={Hall1} alt="hall1" className='w-96' />
-                        <div>
-                            <h2>Location: Building B</h2>
-                            <p>Capacity: 800</p>
-                            <p>Status: Booked</p>
-                        </div>
-                    </div>
-                    <div className='border-2 border-gray-600 p-2 rounded-sm'>
-                        <img src={Hall2} alt="hall2" className='w-96' />
-                        <div>
-                            <h2>Location: Building A</h2>
-                            <p>Capacity: 1000</p>
-                            <p>Status: Vacant</p>
-                        </div>
-                    </div>
-                    <div className='border-2 border-gray-600 p-2 rounded-sm'>
-                        <img src={room2} alt="room2" className='w-96' />
-                        <div>
-                            <h2>Location: Building A</h2>
-                            <p>Capacity: 1000</p>
-                            <p>Status: Vacant</p>
-                        </div>
-                    </div>
-                    <div className='border-2 border-gray-600 p-2 rounded-sm'>
-                        <img src={Hall3} alt="hall3" className='w-96' />
-                        <div>
-                            <h2>Location: Building B</h2>
-                            <p>Capacity: 2000</p>
-                            <p>Status: Booked</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
